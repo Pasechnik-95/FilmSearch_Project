0af08e7a314151e2b9ba83b35d59a46c82dd2917
Admiral <pasechnik-95@yandex.ru>
2024-01-25 02:00:30 +0300

    Add banners at the top view. Add buttons with decorations,ripples, background and toasts. Pluged Bindind instead of synthetics

--- app/src/main/java/com/example/project_moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/project_moviesearch/MainActivity.kt
@@ -2,12 +2,38 @@ package com.example.project_moviesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.project_moviesearch.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initMenuButtons()
+    }
+
+}
+
+private fun initMenuButtons() {
+    binding.menuButton.setOnClickListener {
+        Toast.makeText(it.context, "Меню", Toast.LENGTH_SHORT).show()
+    }
+    binding.accountButton.setOnClickListener {
+        Toast.makeText(it.context, "Аккаунт", Toast.LENGTH_SHORT).show()
+    }
+    binding.collectionsButton.setOnClickListener {
+        Toast.makeText(it.context, "Коллекция", Toast.LENGTH_SHORT).show()
+    }
+    binding.favouritesButton.setOnClickListener {
+        Toast.makeText(it.context, "Избранное", Toast.LENGTH_SHORT).show()
+    }
+    binding.watchLaterButton.setOnClickListener {
+        Toast.makeText(it.context, "Посмотреть позже", Toast.LENGTH_SHORT).show()
     }
 
 
 }
+
